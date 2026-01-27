657209037781486d50cd5f568dbdb3073f8590f9
Nileshsharma0009 <Nileshsharma13416@gmail.com>
2026-01-27 22:33:18 +0530

    bacckend connection on deployment and cors errro

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:1234"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:1234", "https://restaurantyumrush.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
